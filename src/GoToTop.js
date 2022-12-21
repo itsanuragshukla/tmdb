04767685f6b454187d79a7bf5fd866c87bdaba47
theanuragshukla <tachyonanurag@gmail.com>
2022-12-21 18:13:21 +0530

    improved goToTop size, mediaQueries and animation

--- src/GoToTop.js
+++ src/GoToTop.js
@@ -28,7 +28,7 @@ const GoToTop = () => {
         <Wrapper>
             {visible && (
                 <div className="top-btn" onClick={goToTop}>
-                    <FaArrowUp className="top-btn--icon"  style={{fill: "#00fff5"}}/>
+                    <FaArrowUp className="up-icon"  style={{fill: "#00fff5"}}/>
                 </div>
             )}
         </Wrapper>
@@ -41,27 +41,38 @@ const Wrapper = styled.div`
   align-items: center;
   position: relative;
   .top-btn {
-    font-size: 1.8rem;
-    width: 4.6rem;
-    height: 4.6rem;
+    font-size: 1.5rem;
+    width: 50px;
+    height: 50px;
     background-color: #222831;
     border-radius: 50%;
     position: fixed;
-    bottom: 2.5rem;
-    right: 2.5rem;
+    bottom: 35px;
+    right: 25px;
     z-index: 999;
     display: flex;
     justify-content: center;
     align-items: center;
     cursor: pointer;
-    &--icon {
+    & > .up-icon {
+		width:50%;
+		height:50%;
       animation: gototop 1.2s linear infinite alternate-reverse;
     }
     @keyframes gototop {
-      0% {
-        transform: translateY(-0.5rem);
+      50% {
+        transform: translateY(-3px);
       }
+	  100%{
+	  transform:translateY(3px)
+	  }
     }
+
+
+@media only screen and (max-width:200px){
+	display:none;
+}
+
   }
 `;
 
